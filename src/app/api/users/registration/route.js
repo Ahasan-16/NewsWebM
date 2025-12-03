c8f05605fc1a4e1f436f39fc59e9b70d179749ba
Ahasan-16 <mdahasan1916@gmail.com>
2025-12-03 10:23:43 +0600

    creation table&registration api

---
+++ src/app/api/users/registration/route.js
@@ -0,0 +1,20 @@
+import {NextResponse} from "next/server";
+import prisma from "@/lib/prisma";
+
+
+export async function POST(req,res){
+    try{
+        let reqBody=await req.json();
+        reqBody.otp="0";
+
+        const result = await prisma.users.create({
+            data:reqBody
+        });
+        return NextResponse.json({status:"success",data:result});
+
+    }
+    catch (error){
+        return NextResponse.json({staus:"fail",error:error.message});
+    }
+
+}
